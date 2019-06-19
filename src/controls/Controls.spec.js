9f81fcfef9dd8186fdba0d8f723c3bee2daaff37
ParrishJ <jarparrish@gmail.com>
2019-06-19 15:44:07 -0700

    one test for display and controls added

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,26 @@
 // Test away!
+import React from 'react'
+import { render, fireEvent, getByTestId, queryByText } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+import 'jest-dom/extend-expect'
+
+import Controls from '../controls/Controls'
+import Dashboard from '../dashboard/Dashboard'
+
+
+describe('<Controls />', () => {
+    it('Should toggle text on close button', () => {
+        const { getByText } = render(<Dashboard />);
+
+        /* const lockButton = getByTestId(/lockBtn/i); */
+
+        let openBtn = getByText(/Close Gate/i);
+        /* const lockDiv = getByTestId(/lockDiv/i); */
+
+        expect(openBtn).toHaveTextContent('Close Gate');
+
+        fireEvent.click(openBtn);
+
+        expect(openBtn).toHaveTextContent('Open Gate')
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,43 @@
 // Test away!
+import React from 'react'
+import { render, fireEvent, getByTestId, queryByText } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+import 'jest-dom/extend-expect'
+
+import Display from './Display'
+import Controls from '../controls/Controls'
+import Dashboard from '../dashboard/Dashboard';
+
+describe('<Display />', () => {
+    it('Should display either "open" or "closed"', () => {
+        const { getByText } = render(<Dashboard />)
+
+        const closeButton = getByText(/Close Gate/i);
+
+
+
+        const openDiv = getByText(/Open/i);
+
+        expect(openDiv).toHaveTextContent('Open');
+
+        fireEvent.click(closeButton)
+
+        expect(openDiv).toHaveTextContent('Closed')
+    })
+
+    /* it('Should display either "open" or "closed"', () => {
+        const { getByText } = render(<Display />);
+        const { getByTestId } = render(<Controls />)
+
+        const lockButton = getByTestId(/lockBtn/i);
+        const openButton = getByTestId(/openBtn/i);
+
+        fireEvent.click(openButton)
+
+        expect(getByText(/unlocked/i)).toBeTruthy();
+
+        fireEvent.click(lockButton);
+
+        expect(getByText(/locked/i)).toBeTruthy();
+    }) */
+})
